10b02c8389eb5ee7947d928b8d2180272c1b6bc2
Uzziel Perez <uzzie.perez@cern.ch>
2018-05-27 06:30:44 +0200

    cleaning cfg

--- nPhotonAnalyzer/python/ConfFile_cfg.py
+++ nPhotonAnalyzer/python/ConfFile_cfg.py
@@ -20,7 +20,6 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
         #'file:myfile.root'
         inFile
